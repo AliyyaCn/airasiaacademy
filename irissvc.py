8294b670f72dcf3c56d753b30a2d700b278627fb
AliyyaCn <134031058+AliyyaCn@users.noreply.github.com>
2023-12-21 14:52:51 +0800

    Update irissvc.py

--- irissvc.py
+++ irissvc.py
@@ -33,11 +33,11 @@ Y = data.species.copy()
  
 modelsvmc = pickle.load(open("Iris.h5", "rb"))
 
-#st.subheader('Class labels and their corresponding index number')
-#st.write(Y.unique())
+st.subheader('Class labels and their corresponding index number')
+st.write(Y.unique())
 
-st.subheader('Prediction')
-st.write(prediction)
+#st.subheader('Prediction')
+#st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
